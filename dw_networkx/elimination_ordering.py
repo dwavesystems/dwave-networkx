d03a2f415bc637857991695e2100731b6de43f2e
Alexander Condello <acondello@dwavesys.com>
2017-01-09 09:28:11 -0800

    fixed branch and bound. Also the simplicial

--- dw_networkx/elimination_ordering.py
+++ dw_networkx/elimination_ordering.py
@@ -1,8 +1,8 @@
 import itertools
 import networkx as nx
 
-__all__ = ['treewidth_branch_and_bound', 'minor_min_width', 'min_width_heuristic', 'is_simplical',
-           'is_complete', 'is_almost_simplical']
+__all__ = ['treewidth_branch_and_bound', 'minor_min_width', 'min_width_heuristic', 'is_simplicial',
+           'is_complete', 'is_almost_simplicial', 'min_fill_heuristic']
 
 
 def treewidth_branch_and_bound(G):
@@ -15,30 +15,91 @@ def treewidth_branch_and_bound(G):
         treewidth : the treewidth of the graph G
         order : an elimination order that induces the treewidth
     """
-    ub, order = min_width_heuristic(G)  # an upper bound on the treewidth
-    h = minor_min_width(G)  # a lower bound on the treewidth
+    ub, order = min_fill_heuristic(G)  # an upper bound on the treewidth
+    lb = minor_min_width(G)  # a lower bound on the treewidth
 
-    if h == ub:
+    if lb == ub:
         return ub, order
 
-    if ub < h:
+    if ub < lb:
         raise Exception('logic error, upper bound should be greater than h')
 
-    x = []  # the potential better ordering
-    nv = []  # these are the neighbors of the last entry of x, empty right now
-    g = 0
-    return _BB(G, x, nv, ub, order, g, h, {})  # h == f in this case
+    partial_order = []  # the potential better ordering
+    nv = []  # these are the neighbors of v == partial_order[-1], empty for now
+
+    upper_bound = (ub, order)
+    state = (G.copy(), partial_order, nv)
+    info = (lb, 0)  # lb, g
+    return _BB(state, upper_bound, info)
 
 
-def is_simplical(v, G):
-    """Determines whether a vertex v in G is simplical.
-    A vertex is simplical if its neighborhood induces a clique."""
+def _BB(state, upper_bound, info):
+    """helper function for treewidth_branch_and_bound
+    NB: acts on G in place
+    lb == f from paper"""
+
+    G, partial_order, nv = state  # extract the base graph and associated partial order
+    ub, order = upper_bound
+    lb, g = info
+
+    # first up, we can add edges to G according to the following rule:
+    # if |N(v1) and N(v2)| > ub then the final elimination order will require an edge
+    # so let's be proactive and add them now
+    for v1, v2 in itertools.combinations(G, 2):
+        if len(tuple(nx.common_neighbors(G, v1, v2))) > ub:
+            G.add_edge((v1, v2))
+
+    # next we are going to remove some nodes from G and add them to the partial_order
+    sflag = True
+    while sflag:
+        sflag = False
+        for v in G:
+            if is_simplicial(G, v) or (is_almost_simplicial(G, v) and G.degree(v) <= lb):
+                sflag = True
+                partial_order.append(v)
+                g = max(g, G.degree(v))
+                lb = max(g, lb)
+                _elim(G, v)
+                break
+
+    # now the terminal rule
+    if len(G.nodes()) < 1:
+        return min(ub, lb), partial_order
+    if len(G.nodes()) < 2:
+        return min(ub, lb), partial_order+G.nodes()
+
+    for v in G:
+        if v in nv:  # we can skip direct neighbors
+            continue
+
+        # create a new state with v eliminated
+        po_s = partial_order + [v]  # add v to the new partial order
+        nv_s = G[v]  # the neighbors of v
+        G_s = G.copy()  # so we can manipulate Gs
+        _elim(G_s, v)
+
+        new_state = (G_s, po_s, nv_s)
+
+        g_s = max(g, G.degree(v))  # not changed by _elim
+        lb_s = max(g_s, minor_min_width(G_s))
+        new_info = (lb_s, g_s)
+
+        if lb_s < ub:  # we need to keep going
+            upper_bound = _BB(new_state, upper_bound, new_info)
+            ub, order = upper_bound
+
+    return upper_bound
+
+
+def is_simplicial(G, v):
+    """Determines whether a vertex v in G is simplicial.
+    A vertex is simplicual if its neighbors form a clique."""
     return is_complete(G.subgraph(G[v]))
 
 
-def is_almost_simplical(v, G):
-    """determines whether a vertex v in G is almost simplical.
-    A vertex is almost simplical if all but one of its neighbors induce a clique"""
+def is_almost_simplicial(G, v):
+    """determines whether a vertex v in G is almost simplicial.
+    A vertex is almost simplicial if all but one of its neighbors induce a clique"""
     for u in G[v]:
         if is_complete(G.subgraph([w for w in G[v] if w != u])):
             return True
@@ -49,85 +110,6 @@ def is_complete(G):
     """returns true if G is a complete graph"""
     n = len(G.nodes())  # get the number of nodes
     return len(G.edges()) == n*(n-1)/2
-
-
-def _BB(G, x, nv, ub, order, g, f, graph_reductions):
-    if len(G.nodes()) < 2:
-        return min(ub, f), x + G.nodes()  # ub, order
-
-    for v in G:
-        if len(x) > 0 and v in nv:  # we can skip these (section 5.2 in Gogate & Dechter)
-            continue
-
-        # create a new graph by making v simplical then removing it
-        Gs = G.subgraph([n for n in G if n != v])
-        for (n1, n2) in itertools.combinations(G[v], 2):
-            Gs.add_edge(n1, n2)
-        # Gs.remove_node(v)
-
-        # append v to the order
-        xs = x + [v]
-
-        # get the values for our potential ordering
-        gs = max(g, len(G[v]))
-        hs = minor_min_width(Gs)
-        fs = max(gs, hs)
-
-        # we also note that for n1, n2 in G, if |intersection(N(n1), N(n2))| >= ub + 1
-        # we should add an edge between n1 and n2. This will help us in the next step
-        for n1, n2 in nx.non_edges(Gs):
-            if len(list(nx.common_neighbors(Gs, n1, n2))) >= ub + 1:
-                Gs.add_edge((n1, n2))
-
-        # now we want to reduce the graph by eliminating all of the simplical or almost simplical
-        # vertices in Gs
-        # so we want v that are almost simplical and have degree less than lb (hs)
-        # almost simplical means all but one of its neighbors induce a clique
-        Gs, gs, fs, xs = _reduce_graph(Gs, gs, fs, hs, xs, graph_reductions)
-
-        if fs < ub:
-            # update with a better ordering
-            ub, order = _BB(Gs, xs, G[v], ub, order, gs, fs, graph_reductions)
-
-    # best order found so far
-    return ub, order
-
-
-def _reduce_graph(G, g, f, lb, x, graph_reductions):
-    if len(G.nodes()) <= 2:
-        G = nx.Graph()
-        gs = max(g, 1)
-        fs = max(gs, g)
-        xs = x + G.nodes()
-        return G, gs, fs, xs
-
-    G_tuple = tuple(G.nodes())
-    if G_tuple in graph_reductions:
-        return graph_reductions[G_tuple]
-
-    for v in G:
-        if is_simplical(v, G):
-            Gs = G.subgraph([n for n in G if n != v])
-
-            gs = max(g, len(G[v]))
-            fs = max(gs, f)
-            xs = x + [v]
-            graph_reductions[G_tuple] = _reduce_graph(Gs, gs, fs, lb, xs, graph_reductions)
-            return graph_reductions[G_tuple]
-
-        if is_almost_simplical(v, G) and (G.degree(v) <= lb):
-            Gs = G.subgraph([n for n in G if n != v])
-            for (n1, n2) in itertools.combinations(G[v], 2):
-                Gs.add_edge(n1, n2)
-
-            gs = max(g, G.degree(v))
-            fs = max(gs, f)
-            xs = x + [v]
-            graph_reductions[G_tuple] = _reduce_graph(Gs, gs, fs, lb, xs, graph_reductions)
-            return graph_reductions[G_tuple]
-
-    graph_reductions[G_tuple] = (G, g, f, x)
-    return graph_reductions[G_tuple]
 
 
 def minor_min_width(G):
@@ -160,6 +142,43 @@ def minor_min_width(G):
     return lb
 
 
+def _elim(G, v):
+    """eliminated vertex v from G by making it simplicial then removing it.
+    NB: acts on graph in place"""
+    G.add_edges_from(itertools.combinations(G[v], 2))
+    G.remove_node(v)
+
+
+def min_fill_heuristic(G):
+    """computes an upper bound on the treewidth of a graph based on the min-full heuristic
+    for the elimination ordering
+
+    ub, order = min_width(G)
+        G : a NetworkX graph
+        ub : an upper bound on the treewidth of G
+        order : an elimination ordering that induces that upper bound
+    """
+    needed_edges = lambda v: len(list(nx.non_edges(G.subgraph(G[v]))))
+
+    G = G.copy()  # we will be manipulating G
+
+    order = []
+    upper_bound = 0
+
+    while G.nodes():
+
+        # get the node that adds the fewest number of nodes when eliminated from the graph
+        v = min(G.nodes(), key=needed_edges)
+
+        upper_bound = max(upper_bound, G.degree(v))
+
+        order.append(v)
+
+        _elim(G, v)
+
+    return upper_bound, order
+
+
 def min_width_heuristic(G):
     """computes an upper bound on the treewidth of a graph based on the min-width heuristic
     for the elimination ordering.
@@ -180,9 +199,9 @@ def min_width_heuristic(G):
         v = min(degreeG, key=degreeG.get)
 
         # if the number of neighbours of v is higher then upper_bound, update
-        upper_bound = max(upper_bound, len(G[v]))
+        upper_bound = max(upper_bound, G.degree(v))
 
-        # make v simplical by adding edges between each of its neighbors
+        # make v simplicial by adding edges between each of its neighbors
         for (n1, n2) in itertools.combinations(G[v], 2):
             G.add_edge(n1, n2)
 
