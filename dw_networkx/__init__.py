7a6dc9c0287c4dc7c90c04e13f07a6666be53c01
Alexander Condello <acondello@dwavesys.com>
2017-01-11 09:37:10 -0800

    added framework for chimera stucture testing

--- dw_networkx/__init__.py
+++ dw_networkx/__init__.py
@@ -1,2 +1,3 @@
 from main import *
 from elimination_ordering import *
+from chimera_functions import *
---
+++ dw_networkx/chimera_functions.py
@@ -0,0 +1,3 @@
+def is_full_chimera(G):
+    """determines whether a given graph is chimera structured"""
+    raise NotImplementedError('not yet implemented')   
