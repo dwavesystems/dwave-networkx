8ef90cdbefeb5f259a62f531ba62ccdf5f06692b
Alexander Condello <acondello@dwavesys.com>
2017-01-12 13:25:25 -0800

    more chimera stuff, not working

--- dw_networkx/__init__.py
+++ dw_networkx/__init__.py
@@ -1,3 +1,4 @@
+from networkx import *
 from main import *
 from elimination_ordering import *
 from chimera_functions import *
--- dw_networkx/chimera_functions.py
+++ dw_networkx/chimera_functions.py
@@ -1,3 +1,13 @@
-def is_full_chimera(G):
+import networkx as nx
+
+
+def get_chimera_dimensions(G):
     """determines whether a given graph is chimera structured"""
-    raise NotImplementedError('not yet implemented')   
+
+    if not nx.is_bipartite(G):
+        raise ValueError('graph is not chimera-structured')
+
+    L = max(G.degree(G.nodes()).values())
+
+    print len(G)
+    print [l_candidate for l_candidate in range(L, L-2, -1) if (len(G) % (2*l_candidate)) == 0]
