a07ba56dfcc52714a1c9da68542e7fff3da39b0a
Alexander Condello <acondello@dwavesys.com>
2017-05-30 14:33:18 -0700

    updated version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='dwave_networkx',
-    version='0.1',
+    version='1.0',
     packages=['dwave_networkx'],
     install_requires=['networkx>=1.11']
 )
