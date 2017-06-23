f7e0992cb94bd751ae12c4e72c99f83bde648714
Alexander Condello <acondello@dwavesys.com>
2017-06-23 12:12:32 -0700

    unittests now actually check something

--- dwave_networkx/algorithms/tests/test_packing.py
+++ dwave_networkx/algorithms/tests/test_packing.py
@@ -55,22 +55,25 @@ class Solver(object):
 
 class TestPacking(unittest.TestCase):
     def test_maximum_independent_set_basic(self):
+        """Runs the function on some small and simple graphs, just to make
+        sure it works in basic functionality.
+        """
 
         G = dnx.chimera_graph(2, 2, 4)
 
         indep_set = dnx.maximum_independent_set_qa(G, Solver())
-
-        indep_set
+        self.set_independence_check(G, indep_set)
 
         G = dnx.path_graph(5)
         indep_set = dnx.maximum_independent_set_qa(G, Solver())
-
-        indep_set
+        self.set_independence_check(G, indep_set)
 
 #######################################################################################
 # Helper functions
 #######################################################################################
 
-    def set_independence_check(G, indep_set):
-
-        pass
+    def set_independence_check(self, G, indep_set):
+        """Check that the given set of nodes are in fact nodes in the graph and
+        independent of eachother (that is there are no edges between them"""
+        subG = G.subgraph(indep_set)
+        self.assertTrue(len(subG.edges()) == 0)
