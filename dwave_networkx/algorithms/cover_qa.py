79fb4bc117f05750038db754160d107a5fb4a68a
Alexander Condello <acondello@dwavesys.com>
2017-06-26 10:03:37 -0700

    docstring for vertex cover

--- dwave_networkx/algorithms/cover_qa.py
+++ dwave_networkx/algorithms/cover_qa.py
@@ -6,7 +6,57 @@ __all__ = ['min_vertex_cover_qa']
 
 
 def min_vertex_cover_qa(G, solver, **solver_args):
-    """TODO
+    """Tries to determine a minimum vertex cover using the provided
+    quantum annealing (qa) solver.
+
+    A vertex cover is a set of verticies such that each edge of the graph
+    is incident with at least one vertex in the set. A minimum vertex cover
+    is the vertex cover of smallest size.
+
+    Parameters
+    ----------
+    G : NetworkX graph
+
+    solver
+        A quantum annealing solver. Expectes the solver to have a
+        "solve_unstructured_qubo" method that returns a single
+        solution of the form {node: spin, ...}.
+
+    Additional keyword parameters are passed to the given solver.
+
+    Returns
+    -------
+    vertex_cover : list
+       List of nodes that the form a the minimum vertex cover, as
+       determined by the quantum annealer.
+
+    Raises
+    ------
+    TypeError
+        If the provided solver does not have a
+        "solve_unstructured_qubo" method, an exception is raised.
+
+    Examples
+    --------
+    >>> G = dnx.chimera_graph(2, 2, 4)
+    >>> dnx.min_vertex_cover_qa(G, Solver())
+    [0, 1, 2, 3, 12, 13, 14, 15, 20, 21, 22, 23, 24, 25, 26, 27]
+
+    Notes
+    -----
+    Quantum annealers by their nature do not necessarily return correct
+    answers. This function makes no attempt to check the quality of the
+    solution.
+
+    https://en.wikipedia.org/wiki/Vertex_cover
+
+    https://en.wikipedia.org/wiki/Quadratic_unconstrained_binary_optimization
+
+    References
+    ----------
+    .. [1] Lucas, A. (2014). Ising formulations of many NP problems.
+       Frontiers in Physics, Volume 2, Article 5.
+
     """
 
     if not hasattr(solver, "solve_unstructured_qubo") \
@@ -29,7 +79,7 @@ def min_vertex_cover_qa(G, solver, **solver_args):
     Q = {(node, node): B - A*G.degree(node) for node in G}
     Q.update({edge: A for edge in G.edges_iter()})
 
-    # we expect that the solution will be a dict of the form {node: bool}
+    # we expect that the solution will be a dict of the form {node: int(bool)}
     solution = solver.solve_unstructured_qubo(Q, **solver_args)
 
     # nodes that are true are in the cover
