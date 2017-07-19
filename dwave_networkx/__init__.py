d47b5df595fef5c9b4926be4426b45d006950f51
Alexander Condello <acondello@dwavesys.com>
2017-07-19 14:39:59 -0700

    elim orderng moving bug

--- dwave_networkx/__init__.py
+++ dwave_networkx/__init__.py
@@ -3,9 +3,6 @@ from __future__ import absolute_import
 import networkx
 from networkx import *
 
-import dwave_networkx.elimination_ordering
-from dwave_networkx.elimination_ordering import *
-
 import dwave_networkx.architectures
 from dwave_networkx.architectures import *
 
--- dwave_networkx/algorithms/__init__.py
+++ dwave_networkx/algorithms/__init__.py
@@ -3,3 +3,4 @@ from dwave_networkx.algorithms.clique_dw import *
 from dwave_networkx.algorithms.cover_dw import *
 from dwave_networkx.algorithms.matching_dw import *
 from dwave_networkx.algorithms.social_dw import *
+from dwave_networkx.algorithms.elimination_ordering import *
