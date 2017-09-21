861d82d33a6b3a01cddd39cfecc5f9aa1e76c188
Alexander Condello <acondello@dwavesys.com>
2017-09-21 11:26:52 -0700

    updated setup and added config

--- setup.py
+++ setup.py
@@ -1,10 +1,15 @@
 from setuptools import setup, find_packages
 
-from dwave_networkx import __version__
+from dwave_networkx import __version__, __author__, __description__, __authoremail__
+
+packages = ['dwave_networkx',
+            'dwave_networkx.algorithms',
+            'dwave_networkx.utils',
+            'dwave_networkx.drawing']
 
 setup(
     name='dwave_networkx',
     version=__version__,
-    packages=find_packages(),
+    packages=packages,
     install_requires=['networkx>=2.0', 'decorator>=4.1.0']
 )
