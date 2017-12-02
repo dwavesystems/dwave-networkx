58ba0ffff6fac5b5b6e49602174e380910c343fe
Bradley Ellert <bellert@dwavesys.com>
2017-12-01 18:13:49 -0800

    Add min_weighted_vertex_cover() and maximum_weighted_independent_set()
    Wrote weighted variants of min_vertex_cover() and maximum_independent_set()
    Reimplemented unweighted versions by calling weighted functions with weight=None

--- dwave_networkx/algorithms/cover.py
+++ dwave_networkx/algorithms/cover.py
@@ -1,7 +1,66 @@
-from dwave_networkx.algorithms.independent_set import maximum_independent_set
+from dwave_networkx.algorithms.independent_set import maximum_weighted_independent_set
 from dwave_networkx.utils import binary_quadratic_model_sampler
 
-__all__ = ['min_vertex_cover', 'is_vertex_cover']
+__all__ = ['min_weighted_vertex_cover', 'min_vertex_cover', 'is_vertex_cover']
+
+
+@binary_quadratic_model_sampler(2)
+def min_weighted_vertex_cover(G, weight=None, sampler=None, **sampler_args):
+    """Returns an approximate minimum weighted vertex cover.
+
+    Defines a QUBO with ground states corresponding to a minimum
+    vertex cover and uses the sampler to sample from it.
+
+    A vertex cover is a set of vertices such that each edge of the graph
+    is incident with at least one vertex in the set. A minimum vertex cover
+    is the vertex cover of minimum total node weight.
+
+    Parameters
+    ----------
+    G : NetworkX graph
+
+    weight : string, optional (default None)
+        If None, every node has equal weight. If a string, use this node
+        attribute as the node weight. A node without this attribute is
+        assumed to have max weight.
+
+    sampler
+        A binary quadratic model sampler. A sampler is a process that
+        samples from low energy states in models defined by an Ising
+        equation or a Quadratic Unconstrained Binary Optimization
+        Problem (QUBO). A sampler is expected to have a 'sample_qubo'
+        and 'sample_ising' method. A sampler is expected to return an
+        iterable of samples, in order of increasing energy. If no
+        sampler is provided, one must be provided using the
+        `set_default_sampler` function.
+
+    sampler_args
+        Additional keyword parameters are passed to the sampler.
+
+    Returns
+    -------
+    vertex_cover : list
+       List of nodes that the form a the minimum vertex cover, as
+       determined by the given sampler.
+
+    Notes
+    -----
+    Samplers by their nature may not return the optimal solution. This
+    function does not attempt to confirm the quality of the returned
+    sample.
+
+    https://en.wikipedia.org/wiki/Vertex_cover
+
+    https://en.wikipedia.org/wiki/Quadratic_unconstrained_binary_optimization
+
+    References
+    ----------
+    .. [AL] Lucas, A. (2014). Ising formulations of many NP problems.
+       Frontiers in Physics, Volume 2, Article 5.
+
+    """
+    indep_nodes = set(maximum_weighted_independent_set(G, weight, sampler, **sampler_args))
+    return [v for v in G if v not in indep_nodes]
 
 
 @binary_quadratic_model_sampler(1)
@@ -71,8 +130,7 @@ def min_vertex_cover(G, sampler=None, **sampler_args):
        Frontiers in Physics, Volume 2, Article 5.
 
     """
-    indep_nodes = set(maximum_independent_set(G, sampler, **sampler_args))
-    return [v for v in G if v not in indep_nodes]
+    return min_weighted_vertex_cover(G, None, sampler, **sampler_args)
 
 
 def is_vertex_cover(G, vertex_cover):
