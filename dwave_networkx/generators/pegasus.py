d4b7b212aa43971fb9001010fda86f6f0ac97fdb
Kelly Boothby <boothby@dwavesys.com>
2018-01-12 15:16:22 -0800

    added a little documentation and a warning so we can pre-release to customers

--- dwave_networkx/generators/pegasus.py
+++ dwave_networkx/generators/pegasus.py
@@ -6,6 +6,7 @@ import networkx as nx
 
 from dwave_networkx import _PY2
 from dwave_networkx.exceptions import DWaveNetworkXException
+import warnings
 
 __all__ = ['pegasus_graph']
 
@@ -16,8 +17,47 @@ if _PY2:
 
 def pegasus_graph(m, create_using=None, node_list=None, edge_list=None, data=True, offset_lists=None, offsets_index=None, coordinates=False):
     """
-    Creates a Pegasus graph of size (m, m).
+    Creates a Pegasus graph of size m.
+
+    TODO : define the topology, explain the coordinate system
+
+    Parameters
+    ----------
+    m : int
+        The number of rows in the Chimera lattice.
+    n : int, optional (default m)
+        The number of columns in the Chimera lattice.
+    t : int, optional (default 4)
+        The size of the shore within each Chimera tile.
+    create_using : Graph, optional (default None)
+        If provided, this graph is cleared of nodes and edges and filled
+        with the new graph. Usually used to set the type of the graph.
+    node_list : iterable, optional (default None)
+        Iterable of nodes in the graph. If None, calculated from (m, n, t).
+        Note that this list is used to remove nodes, so any nodes specified
+        not in range(m * n * 2 * t) will not be added.
+    edge_list : iterable, optional (default None)
+        Iterable of edges in the graph. If None, edges are generated as
+        described above. The nodes in each edge must be integer-labeled in
+        range(m * n * t * 2).
+    data : bool, optional (default True)
+        If True, each node has a chimera_index attribute. The attribute
+        is a 4-tuple Pegasus index as defined above. (if coordinates = True,
+        we set a linear_index, which is an integer)
+    coordinates : bool, optional (default False)
+        If True, node labels are 4-tuple Pegasus indices
+    offset_lists : pair of lists, optional (default None)
+        Used to directly control the shift offsets, each list in the pair
+        should have length 12, and contain ints from {2, 6, 10}.  If
+        offset_lists is not None, then offsets_index must be None.
+    offsets_index : int, optional (default None)
+        A number between 0 and 7 inclusive, to select one of eight shifts
+        under consideration.  If both offsets_index and offset_lists are
+        None, then we set offsets_index = 0.  At least one of these two
+        parameters must be None.
     """
+    warnings.warn("The Pegasus topology is under active research and development, and details may change before public release.")
+
     if offset_lists is None:
         offsets_descriptor = offsets_index = offsets_index or 0
         offset_lists = [
