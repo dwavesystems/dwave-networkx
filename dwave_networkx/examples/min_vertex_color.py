0ae843417c810fdaaca7da5e33df032b360e56c4
Joel M. Gottlieb <joel.gottlieb@gmail.com>
2018-02-07 12:19:56 -0500

    Updating for Python 2/Python 3 compatibility

--- dwave_networkx/examples/min_vertex_color.py
+++ dwave_networkx/examples/min_vertex_color.py
@@ -1,3 +1,4 @@
+from __future__ import print_function
 import dwave_networkx as dnx
 import networkx as nx
 import dimod
@@ -13,6 +14,6 @@ G.add_edges_from([(0,1), (0,2), (1,2), (1,3), (2,3), (1,4), (2,4), (3,4), (3,5),
 # length 6
 candidate = dnx.min_vertex_coloring(G, sampler)
 if dnx.is_vertex_coloring(G, candidate) and len(candidate) == 6:
-  print str(candidate) + " is a minimum vertex coloring"
+  print(candidate, " is a minimum vertex coloring")
 else: 
-  print str(candidate) + " is not a minimum vertex coloring"
+  print(candidate, " is not a minimum vertex coloring")
