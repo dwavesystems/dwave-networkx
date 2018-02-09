ff2f66f9fae4f8365ded85289ae7bbf83668f7c8
Bradley Ellert <bellert@dwavesys.com>
2018-02-08 16:25:59 -0800

    Remove dependency on recommonmark
    Only needed for .md and we're sticking to .rst

--- docs/conf.py
+++ docs/conf.py
@@ -47,8 +47,7 @@ autosummary_generate = True
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
 #
-source_suffix = ['.rst', '.md']
-source_parsers = {'.md': 'recommonmark.parser.CommonMarkParser'}
+source_suffix = ['.rst']
 
 # The master toctree document.
 master_doc = 'index'
