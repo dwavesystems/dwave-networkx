e15a2409325766de503b0ecce559aaa97b35ec8b
Joel M. Gottlieb <joel.gottlieb@gmail.com>
2018-02-22 17:07:52 -0800

    PEP8-ing

--- dwave_networkx/examples/chimera_graph.py
+++ dwave_networkx/examples/chimera_graph.py
@@ -2,6 +2,6 @@ import matplotlib.pyplot as plt
 import dwave_networkx as dnx
 import networkx as nx
 
-G = dnx.chimera_graph(2,2,4)
+G = dnx.chimera_graph(2, 2, 4)
 dnx.draw_chimera(G)
 plt.show()
--- dwave_networkx/examples/min_maximal_matching.py
+++ dwave_networkx/examples/min_maximal_matching.py
@@ -5,12 +5,12 @@ import dimod
 # Use basic simulated annealer
 sampler = dimod.SimulatedAnnealingSampler()
 
-G = dnx.chimera_graph(1,1,4)
+G = dnx.chimera_graph(1, 1, 4)
 # Get the minimum maximal matching, which is known in this case to be of
-# length 4 
+# length 4
 candidate = dnx.min_maximal_matching(G, sampler)
 
 if dnx.is_maximal_matching(G, candidate) and len(candidate) == 4:
-  print (candidate, " is a minimum maximal matching")
-else: 
-  print (candidate, " is not a minimum maximal matching")
+    print (candidate, " is a minimum maximal matching")
+else:
+    print (candidate, " is not a minimum maximal matching")
