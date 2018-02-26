1cd406c474aba05656ee3e09302255a7c96e6d32
Joel M. Gottlieb <joel.gottlieb@gmail.com>
2018-02-26 16:24:32 -0500

    Python 2/Python 3 compliance in print

--- dwave_networkx/examples/max_cut.py
+++ dwave_networkx/examples/max_cut.py
@@ -12,6 +12,6 @@ G.add_edges_from([(0, 1), (0, 2), (1, 2), (1, 3), (2, 3), (1, 4), (2, 4), (3, 4)
 # Get the max cut
 candidate = dnx.maximum_cut(G, sampler)
 if len(candidate) == 3:
-    print str(candidate) + " is the right length"
+    print (candidate, " is the right length")
 else:
-    print str(candidate) + " is not the right length"
+    print (candidate, " is not the right length")
