506ac7ca992b5df04474486b59e67985997a3f92
Alexander Condello <acondello@dwavesys.com>
2018-12-27 13:31:24 -0800

    Add apache 2.0 headers

--- dwave_networkx/drawing/distinguishable_colors.py
+++ dwave_networkx/drawing/distinguishable_colors.py
@@ -1,3 +1,18 @@
+# Copyright 2018 D-Wave Systems Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+#
+# ================================================================================================
 def distinguishable_color_map(num_colors):
     # Creates a matplotlib Colormap with num_colors visually distinct colors.
     try:
