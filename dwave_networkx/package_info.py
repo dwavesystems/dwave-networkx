083783b476e00a4c91caa77348a47f699728abb1
Alexander Condello <arcondello@gmail.com>
2020-02-24 06:33:03 -0800

    Update version 0.8.3 -> 0.8.4
    Fixes
    -----
    * Remove deprecated calls to translator functions
    * Fixes bad midpoint calculation in `draw_qubit_graph`
    * Fix QUBO generation for TSP
    * Misc doc fixes
    
    
    New Features
    ------------
    * Add ability to plot overlapped embeddigs

--- dwave_networkx/package_info.py
+++ dwave_networkx/package_info.py
@@ -13,7 +13,7 @@
 #    limitations under the License.
 #
 # ================================================================================================
-__version__ = '0.8.3'
+__version__ = '0.8.4'
 __author__ = 'D-Wave Systems Inc.'
 __authoremail__ = 'acondello@dwavesys.com'
 __description__ = 'A NetworkX extension providing graphs and algorithms relevent to working with the D-Wave System'
